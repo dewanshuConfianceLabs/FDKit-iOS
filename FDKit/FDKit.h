d784f89fa6efff17d1ba5cba458aa0e46e20e26c
Dewanshu Sharma <dewanshu.sharma@kindredgroup.com>
2020-07-30 14:36:36 +0200

    Created FDKit

---
+++ FDKit/FDKit.h
@@ -0,0 +1,19 @@
+//
+//  FDKit.h
+//  FDKit
+//
+//  Created by Dewanshu Sharma on 2020-07-30.
+//  Copyright © 2020 Dewanshu Sharma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FDKit.
+FOUNDATION_EXPORT double FDKitVersionNumber;
+
+//! Project version string for FDKit.
+FOUNDATION_EXPORT const unsigned char FDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDKit/PublicHeader.h>
+
+
---
+++ FDKit/FDKit.swift
@@ -0,0 +1,19 @@
+//
+//  FDKit.swift
+//  FDKit
+//
+//  Created by Dewanshu Sharma on 2020-07-30.
+//  Copyright © 2020 Dewanshu Sharma. All rights reserved.
+//
+
+import Foundation
+
+public class FDKit {
+
+    public private(set) var userId: String
+
+    public init(with userId: String) {
+        self.userId = userId
+    }
+
+}
